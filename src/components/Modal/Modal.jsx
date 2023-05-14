b893aed8414756ea815f5991bd4353adc98d8438
olgachibis <olgachibis1968@gmail.com>
2023-05-14 07:43:05 +0300

    react-hw-03-image-finger-14.05.23-1

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import { Component } from 'react'; 
+import { createPortal } from 'react-dom'; 
+import css from './Modal.module.css'; 
+
+const modalRoot = document.querySelector('#modal-root');
+
+export class Modal extends Component {
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.keyDown); 
+  }
+
+  keyDown = e => {
+      if (e.code === 'Escape') {
+      this.props.closeModal(); 
+    }
+  };
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.keyDown); 
+  }
+
+  handleClose = (e) => {
+
+      if (e.currentTarget === e.target) {
+      this.props.closeModal(); 
+    }
+  }
+
+  render() {
+    return createPortal(<div onClick={this.handleClose} className={css.Overlay}>
+      <div className={css.Modal}>{this.props.children}</div> {}
+    </div>, modalRoot)
+  }
+}
